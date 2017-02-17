8dda51b77de012dcea78d526b619ab7e7462866a
Christian Huber <huberc@butterbrot.org>
2017-02-17 01:05:18 +0100

    Generified filter logic

--- src/drawcharts.js
+++ src/drawcharts.js
@@ -1,8 +1,8 @@
-var barWidth = 10;
-var barPadding = 5;
-var xAxisMargin = 5;
-var noWinHeight = 1;
-var barHeight = function(maxHeight, percentage, games) {
+let barWidth = 10;
+let barPadding = 5;
+let xAxisMargin = 5;
+let noWinHeight = 1;
+let barHeight = function(maxHeight, percentage, games) {
     if (percentage>0) {
         return percentage*maxHeight;
     } else if (games > 0) {
@@ -12,77 +12,91 @@ var barHeight = function(maxHeight, percentage, games) {
     }
 }
 
-var barYPos = function(maxHeight, percentage, games) {
+let barYPos = function(maxHeight, percentage, games) {
     return maxHeight - barHeight(maxHeight, percentage, games);
 }
 
-var tickXPos = function(index, width, size) {
+let tickXPos = function(index, width, size) {
     return index*width/size;
 }
 
-var createEmptyEntry = function() {
+let createEmptyEntry = function() {
     return {
             percentage:0,
             games:0
     }
 }
 
-var drawChart = function(ratios) {
-            var values = ratios.values()
-            var keys = ratios.keys()
+let drawChart = function(ratios) {
+            let values = ratios.values()
+            let keys = ratios.keys()
             values.unshift(createEmptyEntry());
-            keys.unshift("");
+            keys.unshift('');
 
-            var height = 100;
-            var width = values.length * (barWidth + barPadding);
-
-
-            var svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
-
-            svg.selectAll("rect").data(values).enter().append("rect")
-                .attr("width", barWidth)
-        	    .attr("height", function(value){return barHeight(height, value.percentage, value.games)})
-        	    .attr("y", function(value){ return barYPos(height, value.percentage, value.games) } )
-        	    .attr("x", function(value, index){return tickXPos(index, width, values.length)-barWidth/2});
+            let height = 100;
+            let width = values.length * (barWidth + barPadding);
 
 
-            var xRange = d3.range(values.length +1).map(function(index){return tickXPos(index, width, values.length)})
-            var xScale = d3.scaleOrdinal(xRange).domain(keys);
-            var xAxis = d3.axisBottom(xScale);
-            svg.append("g")
-                .attr("transform", "translate(0, "+height+")")
-                .call(xAxis).selectAll("text")
-                .style("text-anchor", "end")
-                .attr("dx", "-.8em")
-                .attr("dy", ".15em")
-                .attr("transform", "rotate(-55)");
+            let svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
 
-            var yRange = d3.range(5).map(function(d){return (4-d)*height/4});
-            var yScale = d3.scaleOrdinal().range(yRange).domain([0,25,50,75,100]);
-            var yAxis = d3.axisLeft(yScale);
-            svg.append("g").call(yAxis);
+            svg.selectAll('rect').data(values).enter().append('rect')
+                .attr('width', barWidth)
+        	    .attr('height', function(value){return barHeight(height, value.percentage, value.games)})
+        	    .attr('y', function(value){ return barYPos(height, value.percentage, value.games) } )
+        	    .attr('x', function(value, index){return tickXPos(index, width, values.length)-barWidth/2});
+
+
+            let xRange = d3.range(values.length +1).map(function(index){return tickXPos(index, width, values.length)})
+            let xScale = d3.scaleOrdinal(xRange).domain(keys);
+            let xAxis = d3.axisBottom(xScale);
+            svg.append('g')
+                .attr('transform', 'translate(0, '+height+')')
+                .call(xAxis).selectAll('text')
+                .style('text-anchor', 'end')
+                .attr('dx', '-.8em')
+                .attr('dy', '.15em')
+                .attr('transform', 'rotate(-55)');
+
+            let yRange = d3.range(5).map(function(d){return (4-d)*height/4});
+            let yScale = d3.scaleOrdinal().range(yRange).domain([0,25,50,75,100]);
+            let yAxis = d3.axisLeft(yScale);
+            svg.append('g').call(yAxis);
 }
 
 
-var draw = function() {
-  var dataset;
+let draw = function() {
+  let dataset;
   d3.csv('eurogames.csv', function(data) {
           dataset = data
-           var filteredData = dataset.filter(function(ratio){
 
-                  return ratio.tournament == 'EuroBowl - 2003' && ratio.race == 'Amazons';
-              })
+          let filterMap = d3.map();
+          filterMap.set('year', undefined);
+          filterMap.set('tournament', ['EuroBowl - 2003']);
+          filterMap.set('race', ['Amazons']);
+          filterMap.set('opponentrace', []);
+          let filteredData = dataset.filter(function(ratio){
 
-            var ratios = d3.map();
+              let accept = true;
+              filterMap.entries().forEach(function(filter){
+
+                  if (filter.value != undefined && filter.value.length > 0 && !filter.value.includes(ratio[filter.key])) {
+                    accept = false;
+                  }
+                })
+    
+              return accept;
+           })
+
+            let ratios = d3.map();
 
             filteredData.forEach(function(singleRatio){
-                var field = 'race';
+                let field = 'race';
                 console.log('singleRatio: ' + singleRatio[field]+ ' vs ' + singleRatio.opponentrace)
 
                 if (!ratios.has(singleRatio.opponentrace)) {
-                    ratios.set(singleRatio.opponentrace, {'games':0, 'wins':0, 'draws':0, 'losses':0})
+                    ratios.set(singleRatio.opponentrace, {'games':0, 'wins':0, 'draws':0, 'losses':0, 'isRatio': true})
                 }
-                var ratio = ratios.get(singleRatio.opponentrace)
+                let ratio = ratios.get(singleRatio.opponentrace)
                 ratio.games += +singleRatio.games
                 ratio.wins += +singleRatio.wins
                 ratio.draws += +singleRatio.draws
