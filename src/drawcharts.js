6ed2b26c7b9aa4b3b7d236be340885513aca90cd
Christian Huber <huberc@butterbrot.org>
2017-02-18 01:28:25 +0100

    Added tick pos calculation.

--- src/drawcharts.js
+++ src/drawcharts.js
@@ -2,6 +2,7 @@ const barWidth = 10;
 const barPadding = 5;
 const xAxisMargin = 5;
 const noWinHeight = 1;
+const graphHeight = 100;
 
 let barHeight = function(maxHeight, percentage, games) {
     if (percentage>0) {
@@ -28,37 +29,61 @@ let createEmptyEntry = function() {
     }
 }
 
-let drawChart = function(ratios) {
+let populateTickPos = function(ratios, splitFields, tickPos, offset){
+    if (splitFields.length > 0) {
+        let ticks = tickPos.get(splitFields[0]);
+        console.log(JSON.stringify(ticks))
+        ratios.values().forEach(function(value){
+            offset = populateTickPos(value, splitFields.slice(1), tickPos, offset) + barPadding
+            ticks.push(offset)
+        })
+    } else {
+        offset += barPadding + barWidth
+    }
+
+    return offset;
+}
+
+let drawChart = function(ratios, splitFields) {
             let values = ratios.values()
             let keys = ratios.keys()
             values.unshift(createEmptyEntry());
             keys.unshift('');
 
-            let height = 100;
-            let width = values.length * (barWidth + barPadding);
+            let tickPos = d3.map();
+            splitFields.forEach(function(field){
+                tickPos.set(field, []);
+            })
 
 
-            let svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
+
+            let width = populateTickPos(ratios, splitFields, tickPos, 0);
+
+            console.log("width:" + width)
+
+            console.log(JSON.stringify(tickPos))
+
+            let svg = d3.select('body').append('svg').attr('width', width).attr('height', graphHeight);
 
             svg.selectAll('rect').data(values).enter().append('rect')
                 .attr('width', barWidth)
-        	    .attr('height', function(value){return barHeight(height, value.percentage, value.games)})
+        	    .attr('height', function(value){return barHeight(graphHeight, value.percentage, value.games)})
         	    .attr('y', function(value){ return barYPos(height, value.percentage, value.games) } )
-        	    .attr('x', function(value, index){return tickXPos(index, width, values.length)-barWidth/2});
+        	    .attr('x', function(value, index){return graphHeight(index, width, values.length)-barWidth/2});
 
 
             let xRange = d3.range(values.length +1).map(function(index){return tickXPos(index, width, values.length)})
             let xScale = d3.scaleOrdinal(xRange).domain(keys);
             let xAxis = d3.axisBottom(xScale);
             svg.append('g')
-                .attr('transform', 'translate(0, '+height+')')
+                .attr('transform', 'translate(0, '+graphHeight+')')
                 .call(xAxis).selectAll('text')
                 .style('text-anchor', 'end')
                 .attr('dx', '-.8em')
                 .attr('dy', '.15em')
                 .attr('transform', 'rotate(-55)');
 
-            let yRange = d3.range(5).map(function(d){return (4-d)*height/4});
+            let yRange = d3.range(5).map(function(d){return (4-d)*graphHeight/4});
             let yScale = d3.scaleOrdinal().range(yRange).domain([0,25,50,75,100]);
             let yAxis = d3.axisLeft(yScale);
             svg.append('g').call(yAxis);
@@ -76,7 +101,7 @@ let draw = function() {
           filterMap.set('race', ['Amazons', 'Norse']);
           filterMap.set('opponentrace', []);
 
-          let splitFields = ["year", "tournament", "race"];
+          let splitFields = ["year", "tournament", "race", "opponentrace"];
 
           let filteredData = dataset.filter(function(ratio){
 
@@ -100,7 +125,7 @@ let draw = function() {
             })
 
             console.log(JSON.stringify(ratios))
-            drawChart(ratios)
+            drawChart(ratios, splitFields)
 
           });
 }
